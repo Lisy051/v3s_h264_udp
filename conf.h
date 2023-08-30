3dea31207c3a1ad987b59e803f984402bb65501d
Lisy051 <2847797925@qq.com>
2023-08-31 00:51:42 +0800

    【修改】适配新设备树

--- conf.h
+++ conf.h
@@ -15,7 +15,7 @@
 #define G_HEIGHT 1080
 #define G_V4L2_PIX_FMT V4L2_PIX_FMT_NV12
 #define G_FPS 30
-#define G_SUBDEV_ENTITY_NAME "ov5640 0-003c"
+#define G_SUBDEV_ENTITY_NAME "ov5640 2-003c"
 
 #define G_FRAMES 450	// 15 seconds at 30fps
 
